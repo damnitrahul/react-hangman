b5673b755b77e6e34275ec5aa4133bd5ac92c8ef
Rahul Raj <damnitrahul@gmail.com>
2020-03-29 20:27:16 +0530

    Added console log

--- src/App.js
+++ src/App.js
@@ -3,6 +3,27 @@ import './App.css';
 import Hangman from './Hangman';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log(
+      '%c 👋 Oh, Hi there, Web lover!',
+      'color: #d74034; font-size:24px;'
+    );
+    console.log(
+      `%c
+    ✨ Like what you see? We could work together!
+      
+    🐛 Found a bug? Please, let me know by e-mail, twitter, github issue...
+      
+    📬 Find me here: https://twitter.com/damnitrahul
+      
+    Let the debug begin!
+    
+    Bonus meme: https://i.redd.it/2k1ut81gpcl01.png
+    `,
+      'font-size:16px'
+    );
+  }
   render() {
     return (
       <div className="App">
